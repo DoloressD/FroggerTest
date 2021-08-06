191a40ddb2e835801cb2d71cb3f5b26aee7c2112
DoloressD <sarah.omar.a.a@gmail.com>
2021-08-05 18:14:26 -0700

    Uploaded

---
+++ Frogger Test/Assets/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private static GameObject gbInstance;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+        if (gbInstance == null)
+            gbInstance = this.gameObject;
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 120;
+    }
+}
